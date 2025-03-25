e4b021b25d3669d656a80151ef4ae53a46050cb4
Igor Rangel <igordrangel@gmail.com>
2025-03-25 19:02:00 -0300

    ajustes

--- app-demo/README.md
+++ app-demo/README.md
@@ -2,4 +2,4 @@
   <a href="https://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="120" alt="Nest Logo" /></a>
 </p>
 
-<h1 align="center">[projectName]</h1>
+<h1 align="center">app-demo</h1>
--- src/commands/new-project/index.ts
+++ src/commands/new-project/index.ts
@@ -1,12 +1,23 @@
 import { copyFolder } from '@/utils/copy-folder'
 import chalk from 'chalk'
 import { execSync } from 'node:child_process'
+import { readFileSync, writeFileSync } from 'node:fs'
 import path from 'node:path'
 
 export function newProject(projectName: string) {
   copyFolder(path.join(__dirname, 'code-base/startup-project'), projectName)
+  
+  const packageJson = require(path.join(process.cwd(), projectName, 'package.json'))
+  packageJson.name = projectName
+  writeFileSync(path.join(process.cwd(), projectName, 'package.json'), JSON.stringify(packageJson, null, 2))
+
+  let readme = readFileSync(path.join(path.join(process.cwd(), projectName, 'README.md'))).toString()
+  readme = readme.replace('[projectName]', projectName)
+  writeFileSync(path.join(process.cwd(), projectName, 'README.md'), readme)
+
   execSync(`cd ${projectName} && npm install && npx prisma generate`, {
     stdio: 'inherit',
   })
+
   console.log(`${chalk.green('Projeto criado com sucesso!')}`)
 }
