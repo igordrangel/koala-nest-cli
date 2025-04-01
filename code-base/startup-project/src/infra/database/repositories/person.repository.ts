32102bcd82e0779eada782020adb693b1a436a27
Igor Rangel <igordrangel@gmail.com>
2025-04-01 14:31:03 -0300

    atualizando code-base de startup-project e koala-nest

--- code-base/startup-project/src/infra/database/repositories/person.repository.ts
+++ code-base/startup-project/src/infra/database/repositories/person.repository.ts
@@ -1,7 +1,7 @@
 import { ReadManyPersonDto } from '@/domain/dtos/read-many-person.dto'
 import { Person } from '@/domain/entities/person/person'
 import { IPersonRepository } from '@/domain/repositories/iperson.repository'
-import { CreatedRegistreResponseBase } from '@koalarx/nest/core/controllers/created-registre-response.base'
+import { CreatedRegistreWithIdResponse } from '@koalarx/nest/core/controllers/created-registre-response.base'
 import { ListResponseBase } from '@koalarx/nest/core/controllers/list-response.base'
 import { RepositoryBase } from '@koalarx/nest/core/database/repository.base'
 import { PRISMA_TOKEN } from '@koalarx/nest/core/koala-nest-database.module'
@@ -28,8 +28,14 @@ export class PersonRepository
     })
   }
 
-  save(person: Person): Promise<CreatedRegistreResponseBase<number>> {
-    return this.saveChanges(person)
+  async save(person: Person): Promise<CreatedRegistreWithIdResponse | null> {
+    return this.saveChanges(person).then((response) => {
+      if (response) {
+        return { id: response.id }
+      }
+
+      return null
+    })
   }
 
   read(id: number): Promise<Person | null> {
